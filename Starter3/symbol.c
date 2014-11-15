023afd513d0485c2eb13fa5ebe382e7834d92a2f
eisenjar <eisenjar@ug60.eecg>
2014-11-15 16:16:39 -0500

    fixed vec assignments in symbol table

--- Starter3/symbol.c
+++ Starter3/symbol.c
@@ -137,11 +137,13 @@ symbol_table *build_all_tables(node *ast, symbol_table *current_table) {
     build_all_tables(ast->declaration.expr, ast->current_table);
 	 int decl_type;
 	 if (strcmp(ast->declaration.type->type.name, "int") == 0) decl_type = INT;
-	 else if (strcmp(ast->declaration.type->type.name, "ivec") == 0) decl_type = IVEC4;
+	 else if (strcmp(ast->declaration.type->type.name, "ivec") == 0) decl_type = IVEC2;
 	 else if (strcmp(ast->declaration.type->type.name, "bool") == 0) decl_type = BOOLEAN;
-	 else if (strcmp(ast->declaration.type->type.name, "bvec") == 0) decl_type = BVEC4;
+	 else if (strcmp(ast->declaration.type->type.name, "bvec") == 0) decl_type = BVEC2;
 	 else if (strcmp(ast->declaration.type->type.name, "float") == 0) decl_type = FLOAT;
-	 else if (strcmp(ast->declaration.type->type.name, "vec") == 0) decl_type = VEC4;
+	 else if (strcmp(ast->declaration.type->type.name, "vec") == 0) decl_type = VEC2;
+
+	 if(ast->declaration.type->type.size > 0) decl_type += ast->declaration.type->type.size - 1;
 
 	 /*if(ast->declaration.expr != NULL) {
 		//get the values
